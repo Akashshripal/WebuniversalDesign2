b30ead984d0bc9da3abec181bcc8910c1e4e2df6
Akashshripal <akashshripal786@gmail.com>
2023-06-29 18:01:26 +0530

    Update SideIcons.jsx

--- src/component/SideIcons.jsx
+++ src/component/SideIcons.jsx
@@ -7,13 +7,13 @@ export default function SideIcons() {
             <a href="https://www.facebook.com/webuniversalsindia" target='_blank' className="facebook">
             <i className="fa-brands fa-facebook"></i>
             </a>
-            <a href="https://twitter.com/webuniversals" className="twitter">
+            <a href="https://twitter.com/webuniversals" target='_blank' className="twitter">
                 <i className="fa-brands fa-twitter"></i>
             </a>
-            <a href="https://www.instagram.com/accounts/login/" className="intragram">
+            <a href="https://www.instagram.com/accounts/login/" target='_blank' className="intragram">
                 <i className="fa-brands fa-instagram"></i>
             </a>
-            <a href="https://www.linkedin.com/company/web-universals"  className="linkedin">
+            <a href="https://www.linkedin.com/company/web-universals" target='_blank'  className="linkedin">
                 <i className="fa-brands fa-linkedin"></i>
             </a>
         </div>
